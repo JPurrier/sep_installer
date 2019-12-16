655fc22af0e451dfe33bcf53fd64b2a8f1d22e3a
jay101uk <jerome.purrier@gmail.com>
2019-12-16 16:31:19 +0000

    created push install method

---
+++ Sep_Push_installer.ps1
@@ -0,0 +1,12 @@
+$server_ou = (Get-ADOrganizationalUnit -Filter 'Name -like "Servers"').DistinguishedName 
+$computers = Get-ADComputer -SearchBase $server_ou
+
+
+ForEach($item in $computers)
+{
+    Write-Output "Connecting to: $item"
+   $session =  New-PSSession $item
+   $result = Invoke-Command -Session $remotesession `
+   { Start-Process "\\$((Get-WmiObject win32_computersystem).Domain)\SYSVOL\$((Get-WmiObject win32_computersystem).Domain)\scripts\SEP.exe"  -Wait } 
+    Write-Output $result
+}
